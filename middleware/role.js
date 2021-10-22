4bfd46b69db7060a41ec312df7441afc058042f6
wissmedia <wissmedia@gmail.com>
2021-10-23 07:07:52 +0800

    Update role.js
    add ADMIN_EMAIL to env

--- middleware/role.js
+++ middleware/role.js
@@ -1,6 +1,6 @@
 module.exports = {
   isAdmin: function (req, res, next) {
-    if (req.user.role == 'admin') {
+    if (req.user.email == process.env.ADMIN_EMAIL) {
       return next()
     } else {
       res.redirect('/author')
