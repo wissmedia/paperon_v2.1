2538c990b035b74ac52a89fd83e020c69dc65783
wissmedia <wissmedia@gmail.com>
2021-10-25 01:46:22 +0800

    add method override
    to allow use PUT, PATCH, DELETE from Frontend

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
+const methodOverride = require('method-override')
 
 // IMPORT AUTH AND ROLE MIDDLEWARE
 const { ensureAuth, isAuth } = require('./middleware/auth')
@@ -35,6 +36,18 @@ app.locals.appNames = {
 // USE BODY PARSER
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+// METHOD OVERRIDE (to use PUT and DELETE on front-side js)
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
 
 // LOGGER (if on development mode)
 if (process.env.NODE_ENV === 'development') {
