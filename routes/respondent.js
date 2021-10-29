480140966ce2ab192595048317087c22ea5de671
wissmedia <wissmedia@gmail.com>
2021-10-29 12:24:19 +0800

    update respondent page

--- routes/respondent.js
+++ routes/respondent.js
@@ -1,9 +1,21 @@
 const router = require('express').Router()
+const link = '/respondent'
 
 // @desc    Responden Index Page
 // @route   GET /responden
 router.get('/', (req, res) => {
-  res.render('respondent/index', { navTitle: 'Hello Paperon - Respondent' })
+  let navMenu = [
+    { link: '/', icon: 'fas fa-chevron-circle-left', label: 'Back' },
+  ]
+  let RespoMenu = [
+    { link: `${link}/public`, icon: 'fas fa-bug', label: 'Public' },
+    { link: `${link}/code`, icon: 'fas fa-bug', label: 'Use Code' },
+  ]
+  res.render('respondent/index', { 
+    navTitle: 'Respondent Panel',
+    navMenu, 
+    RespoMenu
+  })
 })
 
 module.exports = router
